549e8d83f8ea262eb5743989369331e7094d173d
Spydr06 <spydr06@web.de>
2025-02-17 20:53:57 +0100

    Use discord slash commands instead of normal messages

--- go.mod
+++ go.mod
@@ -2,8 +2,9 @@ module ipbot
 
 go 1.23.5
 
+require github.com/bwmarrin/discordgo v0.28.2-0.20241208071600-33ffff21d31a
+
 require (
-	github.com/bwmarrin/discordgo v0.28.1 // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
 	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
--- ipbot.go
+++ ipbot.go
@@ -7,43 +7,122 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"slices"
-	"syscall"
+    "slices"
     "strings"
 
 	"github.com/bwmarrin/discordgo"
 )
 
-type commandHandler func(*discordgo.Session, *discordgo.MessageCreate) error
-
 var (
+    dmPermission = true
+    contexts = []discordgo.InteractionContextType{
+        discordgo.InteractionContextPrivateChannel,
+        discordgo.InteractionContextGuild,
+        discordgo.InteractionContextBotDM,
+    }
+
+    integrations = []discordgo.ApplicationIntegrationType{
+        discordgo.ApplicationIntegrationGuildInstall,
+        discordgo.ApplicationIntegrationUserInstall,
+    }
+
     apiTokenEnv = os.Getenv("IPBOT_TOKEN")
     authUsersEnv = os.Getenv("IPBOT_AUTH_USERS")
     authUsers []string
 
-    commands = map[string]commandHandler {
-        "!ping": func(s *discordgo.Session, m *discordgo.MessageCreate) error {
-            s.ChannelMessageSend(m.ChannelID, "Pong!")
-            return nil
+    commands = []*discordgo.ApplicationCommand{
+        {
+            Name: "ip",
+            Description: "Returns the public IPv4 Address",
+            DMPermission: &dmPermission,
+            Contexts: &contexts,
+            Type: discordgo.ChatApplicationCommand,
+            IntegrationTypes: &integrations,
         },
-        "!ip": func(s *discordgo.Session, m *discordgo.MessageCreate) error {
+        {
+            Name: "ipv6",
+            Description: "Returns the public IPv6 Address",
+            Contexts: &contexts,
+            DMPermission: &dmPermission,
+            Type: discordgo.ChatApplicationCommand,
+            IntegrationTypes: &integrations,
+        },
+        {
+            Name: "ping",
+            Description: "Pings the server",
+            Contexts: &contexts,
+            DMPermission: &dmPermission,
+            Type: discordgo.ChatApplicationCommand,
+            IntegrationTypes: &integrations,
+        },
+    }
+
+    commandHandlers = map[string]func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+        "ip": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+            if err := authorize("ip", i.Interaction); err != nil {
+                interactionRespond(s, i.Interaction, "Error: " + err.Error())
+                return
+            }
+            
             ip, err := getPublicIPv4()
-            if err == nil {
-                s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Public IP Address: %v", ip))
+            
+            var msg string
+            if err != nil {
+                msg = fmt.Sprintf("Error fetching IPv4: %v", err)
+            } else {
+                msg = fmt.Sprintf("IPv4: %v", ip)
             }
             
-            return err
+            interactionRespond(s, i.Interaction, msg)
         },
-        "!ipv6": func(s *discordgo.Session, m *discordgo.MessageCreate) error {
-            ip, err := getPublicIPv6()
-            if err == nil {
-                s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Public IP Address: %v", ip))
+        "ipv6": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+            if err := authorize("ipv6", i.Interaction); err != nil {
+                interactionRespond(s, i.Interaction, "Error: " + err.Error())
+                return
             }
+
+            ip, err := getPublicIPv6()
             
-            return err
+            var msg string
+            if err != nil {
+                msg = fmt.Sprintf("Error fetching IPv6: %v", err)
+            } else {
+                msg = fmt.Sprintf("IPv6: %v", ip)
+            }
+
+            interactionRespond(s, i.Interaction, msg)
+        },
+        "ping": func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+            interactionRespond(s, i.Interaction, "Pong!")
         },
     }
 )
+
+func authorize(command string, i *discordgo.Interaction) error {
+    user := i.User
+    if user == nil {
+        user = i.Member.User
+    }
+
+    log.Printf("User '%v' (%v) invoked command '%v'", user, user.ID, command)
+
+    if !slices.Contains(authUsers, user.ID) {
+        return fmt.Errorf("'@%v' is not in the sudoers file. This incident will be reported.", user)
+    }
+
+    log.Printf("Authorized User '%v'", user)
+
+    return nil
+}
+
+func interactionRespond(s *discordgo.Session, i *discordgo.Interaction, message string) {
+    s.InteractionRespond(i, &discordgo.InteractionResponse{
+        Type: discordgo.InteractionResponseChannelMessageWithSource,
+        Data: &discordgo.InteractionResponseData{
+            Content: message,
+        },
+    })
+}
 
 func splitUsers(r rune) bool {
     return r == ':' || r == ',' || r == ';'
@@ -64,49 +143,52 @@ func main() {
         log.Fatalln("Error creating Discord session:", err)
     }
 
-    dg.AddHandler(messageCreate)
+    dg.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+        log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+    })
 
-    dg.Identify.Intents = discordgo.IntentsGuildMessages
+    dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+        if h, ok := commandHandlers[i.ApplicationCommandData().Name]; ok {
+            h(s, i)
+        }
+    })
+
+    dg.Identify.Intents = discordgo.IntentsDirectMessages
 
     err = dg.Open()
     if err != nil {
         log.Fatalln("Error opening connection:", err)
     }
+    defer dg.Close()
+
+    log.Printf("Reloading %v (/) commands...", len(commands))
+
+    registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+    for i, v := range commands {
+        cmd, err := dg.ApplicationCommandCreate(dg.State.User.ID, "", v)
+        if err != nil {
+            log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+        }
+        registeredCommands[i] = cmd
+    }
+
+    sc := make(chan os.Signal, 1)
+    signal.Notify(sc, os.Interrupt)
 
     log.Println("IpBot is now running. CTRL+C to exit.")
-    sc := make(chan os.Signal, 1)
-    signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+    
     <- sc
 
-    dg.Close()
+    log.Println("Removing commands...")
+
+/*    for _, v := range registeredCommands {
+        err := dg.ApplicationCommandDelete(dg.State.User.ID, "", v.ID)
+        if err != nil {
+            log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+        }
+    }*/
 
     log.Println("Shutdown.");
-}
-
-func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-    if m.Author.ID == s.State.User.ID {
-        return
-    }
-
-    if !slices.Contains(authUsers, m.Author.ID) {
-        log.Printf("Attempt to call \"%s\" from unauthorized user \"%s\"", m.Content, m.Author.ID)
-        return
-    }
-
-    log.Printf("Authorized user \"%v\"\n", m.Author)
-
-    handler, has := commands[m.Content]
-    if !has {
-        return
-    }
-
-    log.Printf("Running command \"%s\"\n", m.Content)
-
-    err := handler(s, m)
-    if err != nil {
-        log.Println("Error running command:", err)
-        s.ChannelMessageSend(m.ChannelID, fmt.Sprintf("Error running command: %v", err))
-    }
 }
 
 func getPublicIPv4() (string, error) {
